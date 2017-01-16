dd9f9e39ed03608cd8f48c654b75732382ea1f00
Chris Wightman <wightman_christopher@bah.com>
2017-01-16 10:12:19 -0500

    Added some Javascript

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,18 @@ var cardOne="queen"
 var cardTwo="queen"
 var cardThree="king"
 var cardFour="king"
+var message;
+
+if (cardTwo === cardFour) {
+	alert("You found a match!");
+}
+	else if (cardOne === cardTwo) {
+		alert("You found a match!");
+	}
+    else if (cardOne === cardThree) {
+		alert("You found a match!");
+	}
+    else if (cardOne === cardFour) {
+		alert("You found a match!");
+	}
+	else alert("Sorry try again.");
